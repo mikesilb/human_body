672e10976557468765b537379db0440c0143eaa5
Michael Silberstein <mikesilb123@gmail.com>
2017-12-14 15:05:12 -0500

    Added nose functionality

--- nose.rb
+++ nose.rb
@@ -1,4 +1,32 @@
 class Nose
+  attr_accessor :smelling, :blowing_nose, :snorting
   def initialize
+    @smelling = false
+    @blowing_nose = false
+    @snorting = false
+  end
+  def smell
+    @smelling = true
+    @blowing_nose = false
+    @snorting = false
+    puts "You are using your nose.\nThe nose knows!"
+  end
+  def congested
+    self.blow_nose
+  end
+  def isrunning
+    self.blow_nose
+  end
+  def blow_nose
+    @smelling = false
+    @blowing_nose = true
+    @snorting = false
+    puts "Putting that handkerchief to good use!"
+  end
+  def snort
+    @smelling = false
+    @blowing_nose = false
+    @snorting = true
+    puts "You look weird snorting like that!"
   end
 end
